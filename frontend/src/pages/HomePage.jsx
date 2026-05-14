f0dba9d894f9f8709a85f6231676bd285a1af843
m1rabil1z <abhirupmahato488@gmail.com>
2026-05-14 17:06:18 +0530

    feat: Snippet Detailed Page added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI';
 import api from '../lib/axios';
 import toast from 'react-hot-toast'
 import NoteCard from '../components/NoteCard';
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
   const [isRateLimited,setisRateLmited] = useState(false);
@@ -41,11 +42,15 @@ const HomePage = () => {
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+
+
+        {notes.length === 0  && !isRateLimited && !loading && <NotesNotFound /> }
+
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:gridcols-2 lg:grid-cols-3 gap-6'>
             {notes.map(note => (
               <div key={note._id}>
-                <NoteCard  note={note}/>
+                <NoteCard  note={note} setNotes={setNotes}/>
               </div>
             ))}
           </div>
