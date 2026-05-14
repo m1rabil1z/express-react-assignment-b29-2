932bcf5421ba09df48ef6f6462e3efbeab690b6d
m1rabil1z <abhirupmahato488@gmail.com>
2026-05-14 11:06:47 +0530

    feat: Home Page frontend

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-//create a rate limiter that allows 10 requests per 20 seconds
+//create a rate limiter that allows 100 requests per 10 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5,"10 s")
+    limiter: Ratelimit.slidingWindow(100,"10 s")
 });
 
 export default ratelimit
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
+import cors from "cors"
 
 dotenv.config()
 
@@ -12,8 +13,12 @@ const PORT = process.env.PORT || 5001;
 
 
 //middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+}))
 app.use(express.json()); //middleware parse JSON bodies: req.body
 app.use(rateLimiter)
+
 
 
 //our simple middleware
