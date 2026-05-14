bb18b4f1f0581559e8ed4ac7e9188a5edd91768a
m1rabil1z <abhirupmahato488@gmail.com>
2026-05-14 14:25:12 +0530

    feat: Create Page

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 //create a rate limiter that allows 100 requests per 10 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(100,"10 s")
+    limiter: Ratelimit.slidingWindow(10,"10 s")
 });
 
 export default ratelimit
